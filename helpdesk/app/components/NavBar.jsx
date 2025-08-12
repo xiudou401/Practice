dba94fa8ff564e45c6f223a220cb2c6b3a23000f
Will D <diweizhi@gmail.com>
2025-08-12 12:22:57 +1000

    Nav

--- helpdesk/app/components/NavBar.jsx
+++ helpdesk/app/components/NavBar.jsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import Logo from './dojo-logo.png';
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk logo"
+        width={70}
+        placeholder="blur"
+        quality={100}
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
 }
