35260fac9672aae41dbbe0376a79ebefd85b4e1b
Will D <diweizhi@gmail.com>
2025-08-12 13:11:26 +1000

    TicketList

--- helpdesk/app/tickets/page.jsx
+++ helpdesk/app/tickets/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TicketList from './TicketList';
 
 export default function Tickets() {
   return (
     <main>
       <h2>Tickets</h2>
+      <TicketList />
     </main>
   );
 }
