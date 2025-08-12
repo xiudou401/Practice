f406db3dccfbfe29adf04a52bb846f4201571e06
Will D <diweizhi@gmail.com>
2025-08-12 14:50:06 +1000

    getTicket

---
+++ helpdesk/app/tickets/[id]/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+async function getTicket(id) {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`);
+  return res.json();
+}
+
+export default async function Ticket({ params }) {
+  const { id } = params;
+  const ticket = await getTicket(id);
+  console.log(ticket);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
