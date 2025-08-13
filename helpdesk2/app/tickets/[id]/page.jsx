824182fb1322323074add3bfbb29aded9b85066c
Will D <diweizhi@gmail.com>
2025-08-13 14:32:31 +1000

    getTicket

---
+++ helpdesk2/app/tickets/[id]/page.jsx
@@ -0,0 +1,41 @@
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets');
+  const tickets = await res.json();
+  return tickets.map((ticket) => {
+    id: ticket.id;
+  });
+}
+
+const getTicket = async (id) => {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60,
+    },
+  });
+  if (!res.ok) {
+    notFound();
+  }
+  return res.json();
+};
+export default async function Ticket({ params }) {
+  const { id } = params;
+  const ticket = await getTicket(id);
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
