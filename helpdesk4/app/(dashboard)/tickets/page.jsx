e64d72db562bd9d808c747ad37c6aa8f5ad7d4d1
Will D <diweizhi@gmail.com>
2025-08-14 22:57:55 +1000

    getTickets

---
+++ helpdesk4/app/(dashboard)/tickets/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import TicketList from './TicketList';
+
+export default function Tickets() {
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
+    </main>
+  );
+}
