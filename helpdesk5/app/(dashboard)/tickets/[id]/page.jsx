a4bc8888ceaa3a1ff2e4e24c727a61b676139b4b
Will D <diweizhi@gmail.com>
2025-08-15 12:22:36 +1000

    static params

--- helpdesk5/app/(dashboard)/tickets/[id]/page.jsx
+++ helpdesk5/app/(dashboard)/tickets/[id]/page.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets');
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({ id: ticket.id }));
+}
+
 const getTicket = async (id) => {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
