fb02894b1f461b06302305bd48322a28a7746042
Will D <diweizhi@gmail.com>
2025-08-18 09:51:59 +1000

    post

--- helpdesk2/app/api/tickets/route.js
+++ helpdesk2/app/api/tickets/route.js
@@ -6,3 +6,15 @@ export async function GET() {
 
   return NextResponse.json(tickets, { status: 200 });
 }
+
+export async function POST(request) {
+  const ticket = await request.json();
+  const res = await fetch('http://localhost:4000/tickets', {
+    method: 'POST', // use uppercase for clarity
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(ticket),
+  });
+  const newTicket = await res.json();
+
+  return NextResponse.json(newTicket, { status: 200 });
+}
