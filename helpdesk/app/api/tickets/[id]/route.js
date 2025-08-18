09a15cce0f0157abacd167f8b07142df772fa276
Will D <diweizhi@gmail.com>
2025-08-18 10:27:06 +1000

    get one

---
+++ helpdesk/app/api/tickets/[id]/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(_, { params }) {
+  const id = params.id;
+  const res = await fetch(`http://localhost:4000/tickets/${id}`);
+
+  if (!res.ok) {
+    return NextResponse.json(
+      { error: 'cannot find the ticket' },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  const ticket = await res.json();
+
+  return NextResponse.json(ticket, { status: 200 });
+}
