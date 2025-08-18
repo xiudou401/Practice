d08437b16f6a7ccb2a179b34170c03e748bc097d
Will D <diweizhi@gmail.com>
2025-08-18 10:06:37 +1000

    post

--- helpdesk3/app/(dashboard)/[...not-found]/page.jsx
+++ helpdesk3/app/(dashboard)/[...not-found]/page.jsx
@@ -1,4 +1,5 @@
 import { notFound } from 'next/navigation';
+import React from 'react';
 
 export default function NotFound() {
   return notFound();
---
+++ helpdesk3/app/api/tickets/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const res = await fetch('http://localhost:4000/tickets');
+  const tickets = await res.json();
+  return NextResponse.json(tickets, { status: 200 });
+}
+
+export async function POST(request) {
+  console.log(request);
+  const ticket = await request.json();
+  const res = await fetch('http://localhost:4000/tickets', {
+    method: 'POST', // use uppercase for clarity
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(ticket),
+  });
+  const newTicket = await res.json();
+
+  return NextResponse.json(newTicket, {
+    status: 200,
+  });
+}
