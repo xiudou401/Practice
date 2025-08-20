b8ba3c3aa2674998929b7a986d7f5a19700482a0
Will D <diweizhi@gmail.com>
2025-08-20 10:42:05 +1000

    AuthForm

--- helpdesk6/app/(auth)/login/page.jsx
+++ helpdesk6/app/(auth)/login/page.jsx
@@ -1,5 +1,19 @@
+'use client';
 import React from 'react';
+import AuthForm from '../AuthForm';
 
 export default function Login() {
-  return <div>Login</div>;
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <main>
+      <h2 className="text-center">Login</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+
+      {/* {error && <div className="error">{error}</div>} */}
+    </main>
+  );
 }
--- helpdesk6/app/(auth)/signup/page.jsx
+++ helpdesk6/app/(auth)/signup/page.jsx
@@ -1,5 +1,18 @@
+'use client';
 import React from 'react';
+import AuthForm from '../AuthForm';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <h2 className="text-center">Sign up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+
+      {/* {error && <div className="error">{error}</div>} */}
+    </main>
+  );
 }
