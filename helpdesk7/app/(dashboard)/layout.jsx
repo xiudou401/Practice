6af5c74d57039fe50385f2abe7b909819eb176a8
Will D <diweizhi@gmail.com>
2025-08-20 11:30:48 +1000

    getTickets

--- helpdesk7/app/(dashboard)/layout.jsx
+++ helpdesk7/app/(dashboard)/layout.jsx
@@ -2,9 +2,9 @@ import NavBar from '../components/NavBar';
 
 export default function DashboardLayout({ children }) {
   return (
-    <html lang="en">
+    <>
       <NavBar />
-      <body>{children}</body>
-    </html>
+      {children}
+    </>
   );
 }
--- helpdesk7/app/(dashboard)/tickets/page.jsx
+++ helpdesk7/app/(dashboard)/tickets/page.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import TicketList from './TicketList';
 
 export default function Tickets() {
-  return <div>Tickets</div>;
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
+    </main>
+  );
 }
