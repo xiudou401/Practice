c725c185013cbf36c67082d35d5f0151a42903f3
Will D <diweizhi@gmail.com>
2025-08-20 11:37:03 +1000

    get Ticket

--- helpdesk7/app/(dashboard)/tickets/[id]/page.jsx
+++ helpdesk7/app/(dashboard)/tickets/[id]/page.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
 
-export default function Ticket({ params }) {
-  return <div>Ticket {params.id}</div>;
+const getTicket = async (id) => {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60,
+    },
+  });
+  return res.json();
+};
+
+export default async function Ticket({ params }) {
+  const ticket = await getTicket(params.id);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
 }
