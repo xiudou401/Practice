4b17de5701ea227106fe539b6de91aa18d43e996
Will D <diweizhi@gmail.com>
2025-09-04 13:27:39 +1000

    blogList

--- next-jiuye5/src/app/blog/page.tsx
+++ next-jiuye5/src/app/blog/page.tsx
@@ -1,27 +1,11 @@
-'use client';
-import { Avatar, List } from 'antd';
-import React from 'react';
-import { data } from '@/data/index';
-import Link from 'next/link';
+import type { Metadata } from 'next';
+import BlogList from '@/components/blogList';
+
+export const metadata: Metadata = {
+  title: 'Blog List',
+  description: '...',
+};
 
 export default function Page() {
-  return (
-    <List
-      itemLayout="horizontal"
-      dataSource={data}
-      renderItem={(item, index) => (
-        <List.Item>
-          <List.Item.Meta
-            className="!items-center"
-            avatar={
-              <Avatar
-                src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
-              />
-            }
-            title={<Link href={`/blog/${item.id}`}>{item.title}</Link>}
-          />
-        </List.Item>
-      )}
-    />
-  );
+  return <BlogList />;
 }
---
+++ next-jiuye5/src/components/blogList.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { Avatar, List } from 'antd';
+import React from 'react';
+import { data } from '@/data';
+import Link from 'next/link';
+
+export default function BlogList() {
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={(item, index) => (
+        <List.Item>
+          <List.Item.Meta
+            className="!items-center"
+            avatar={
+              <Avatar
+                src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+              />
+            }
+            title={<Link href={`/blog/${item.id}`}>{item.title}</Link>}
+          />
+        </List.Item>
+      )}
+    />
+  );
+}
