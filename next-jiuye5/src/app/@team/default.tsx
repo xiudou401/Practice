07b5f0a912d7e75fa6a63fa635fee7cb4e3430f3
Will D <diweizhi@gmail.com>
2025-09-05 11:37:43 +1000

    vistors

---
+++ next-jiuye5/src/app/@team/default.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-60 rounded-lg bg-teal-500 flex-1 text-white">
+      @team default
+    </div>
+  );
+}
---
+++ next-jiuye5/src/app/default.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="flex mt-6 p-6 bg-gray-500 rounded-lg text-white">
+      @children default
+    </div>
+  );
+}
