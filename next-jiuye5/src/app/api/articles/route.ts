814ffcc419fe75ecdd09596729153bcf0c57d90c
Will D <diweizhi@gmail.com>
2025-09-10 13:15:09 +1000

    get query

--- next-jiuye5/src/app/api/articles/route.ts
+++ next-jiuye5/src/app/api/articles/route.ts
@@ -1,7 +1,37 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import db from '@/db';
 
-export async function GET(request: Request) {}
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const pagenum = Number(searchParams.get('pagenum')) || 1;
+  const pagesize = Number(searchParams.get('pagesize')) || 2;
+  const query = searchParams.get('query') || '';
+
+  const data = db.data.posts;
+
+  let filteredData = query
+    ? data.filter((item) => {
+        const { id, ...rest } = item;
+        return Object.values(rest).some((value) =>
+          value.toLowerCase().includes(query.toLowerCase())
+        );
+      })
+    : data;
+
+  const total = filteredData.length;
+
+  const startIndex = (pagenum - 1) * pagesize;
+  const endIndex = Math.min(startIndex + pagesize, total);
+
+  filteredData =
+    startIndex >= total ? [] : filteredData.slice(startIndex, endIndex);
+
+  return NextResponse.json({
+    code: 0,
+    message: 'success',
+    data: { total, list: filteredData },
+  });
+}
 
 export async function POST(request: Request) {
   const data = await request.json();
